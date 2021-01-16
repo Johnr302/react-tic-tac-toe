c1d8bf3cb86a25f2861d71511a6c7eac97c21b81
Johnr302 <johnathon.regalado@gmail.com>
2021-01-15 19:32:34 -0800

    testing for <Square /> component

--- src/test/Square.test.js
+++ src/test/Square.test.js
@@ -6,19 +6,29 @@ Enzyme.configure({ adapter: New Adapter() });
 
 describe("<Square />", () => {
     it("render a blank <Square /> component", () => {
-
+        const wrapper = mount(<Square src={img} />)
+        const props = wrapper.props();
+        expect(props.src).toBe("blank/img.png");
     })
     it("render a <Square /> with an X", () => {
-
+        const wrapper = mount(<Square src={img} />)
+        const props = wrapper.props();
+        expect(props.src).toBe("X/img.png");
     })
     it("render a <Square /> with an O", () => {
-
+        const wrapper = mount(<Square src={img} />)
+        const props = wrapper.props();
+        expect(props.src).toBe("O/img.png");
     })
     it("disables the square when the square is marked", () => {
-
+        const wrapper = mount(<Square src={img} disabled={true} />)
+        const props = wrapper.props();
+        expect(props.src).toBe(true);
     })
-    it("disables the square from being clicked ", () =>{
-        
+    it("disables the square from being clicked ", () => {
+        const wrapper = mount(<Square src={img} disabled={true} />)
+        const props = wrapper.props();
+        expect(props.disabled).toBe(true);
     })
 
 })
