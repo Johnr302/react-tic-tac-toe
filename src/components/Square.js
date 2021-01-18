22bc1666bd9c4717c07b176b2fb4929017474fd8
Johnr302 <johnathon.regalado@gmail.com>
2021-01-17 19:20:26 -0800

    changes to <Square /> component and Square.test.js all test pass.

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default () => {
-  return <div className="square"></div>;
+export default (props) => {
+  const { imgSrc, disabled } = props;
+  return <div className="square" src={imgSrc} disabled={disabled}></div>;
 };
--- src/test/Square.test.js
+++ src/test/Square.test.js
@@ -1,34 +1,42 @@
 import React from "react";
 import Enzyme, { mount } from "enzyme";
-import App from "../App"
+import Square from "../components/Square";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-Enzyme.configure({ adapter: New Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Square />", () => {
-    it("render a blank <Square /> component", () => {
-        const wrapper = mount(<Square src={img} />)
-        const props = wrapper.props();
-        expect(props.src).toBe("blank/img.png");
-    })
-    it("render a <Square /> with an X", () => {
-        const wrapper = mount(<Square src={img} />)
-        const props = wrapper.props();
-        expect(props.src).toBe("X/img.png");
-    })
-    it("render a <Square /> with an O", () => {
-        const wrapper = mount(<Square src={img} />)
-        const props = wrapper.props();
-        expect(props.src).toBe("O/img.png");
-    })
-    it("disables the square when the square is marked", () => {
-        const wrapper = mount(<Square src={img} disabled={true} />)
-        const props = wrapper.props();
-        expect(props.src).toBe(true);
-    })
-    it("disables the square from being clicked ", () => {
-        const wrapper = mount(<Square src={img} disabled={true} />)
-        const props = wrapper.props();
-        expect(props.disabled).toBe(true);
-    })
-
-})
+  it("render a blank <Square /> component", () => {
+    const imgSrc = "blank/img.png";
+    const wrapper = mount(<Square src={imgSrc} />);
+    console.log(wrapper.debug());
+    const props = wrapper.props();
+    expect(props.src).toBe("blank/img.png");
+  });
+  it("render a <Square /> with an X", () => {
+    const imgSrc = "X/img.png";
+    const wrapper = mount(<Square src={imgSrc} />);
+    const props = wrapper.props();
+    expect(props.src).toBe("X/img.png");
+  });
+  it("render a <Square /> with an O", () => {
+    const imgSrc = "O/img.png";
+    const wrapper = mount(<Square src={imgSrc} />);
+    const props = wrapper.props();
+    expect(props.src).toBe("O/img.png");
+  });
+  it("disables the square when the square is marked", () => {
+    const imgSrc = "blank/img.png";
+    const disabled = true;
+    const wrapper = mount(<Square src={imgSrc} disabled={disabled} />);
+    const props = wrapper.props();
+    expect(props.disabled).toBe(true);
+  });
+  it("disables the square from being clicked ", () => {
+    const imgSrc = "blank/img.png";
+    const disabled = false;
+    const wrapper = mount(<Square src={imgSrc} disabled={disabled} />);
+    const props = wrapper.props();
+    expect(props.disabled).toBe(false);
+  });
+});
